67c766b603540febcaec0f8f224cc6e8ab4d5011
Diego Vilhalva <diegorocha450@yahoo.com>
2026-06-30 15:04:40 -0300

    style: dark theme for auth pages and custom app logo

--- resources/js/Components/Checkbox.jsx
+++ resources/js/Components/Checkbox.jsx
@@ -4,7 +4,7 @@ export default function Checkbox({ className = '', ...props }) {
             {...props}
             type="checkbox"
             className={
-                'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500 ' +
+                'rounded border-gray-700 bg-gray-800 text-blue-600 shadow-sm focus:ring-blue-500 focus:ring-offset-gray-900 ' +
                 className
             }
         />
--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -7,12 +7,12 @@ export default forwardRef(function TextInput(
     const localRef = useRef(null);
 
     useImperativeHandle(ref, () => ({
-        focus: () => localRef.current?.focus(),
+        focus: () => localRef.current.focus(),
     }));
 
     useEffect(() => {
         if (isFocused) {
-            localRef.current?.focus();
+            localRef.current.focus();
         }
     }, [isFocused]);
 
@@ -21,7 +21,7 @@ export default forwardRef(function TextInput(
             {...props}
             type={type}
             className={
-                'rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 ' +
+                'rounded-md bg-gray-800 border-gray-700 text-white placeholder-gray-500 shadow-sm focus:border-blue-500 focus:ring-blue-500 ' +
                 className
             }
             ref={localRef}
